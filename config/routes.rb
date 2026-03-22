72d80751226da94c9fb0ffbe54fc9029b161813d
Ssimran-5 <simranbhumbak22@gmail.com>
2026-03-22 00:09:39 -0500

    created controller hospitals

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get "hospitals/index"
+  get "hospitals/show"
   get "about", to: "pages#about", as: :about
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
