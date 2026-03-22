59236a13d40bab82996fdbd9f02b2f1c6e129e16
Ssimran-5 <simranbhumbak22@gmail.com>
2026-03-21 22:53:34 -0500

    generated seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,57 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+
+require 'faker'
+
+# Hospitals
+15.times do
+  Hospital.create!(
+    name: "#{Faker::Company.name} Hospital",
+    address: Faker::Address.street_address,
+    city: Faker::Address.city,
+    phone: Faker::PhoneNumber.phone_number,
+    latitude: Faker::Address.latitude,
+    longitude: Faker::Address.longitude
+  )
+end
+
+specialties = [
+  "Cardiology", "Neurology", "Pediatrics",
+  "Orthopedics", "Dermatology", "Oncology"
+]
+
+departments = [
+  "Emergency", "Surgery", "Radiology",
+  "ICU", "Outpatient", "Pharmacy"
+]
+
+Hospital.all.each do |hospital|
+  # Doctors
+  8.times do
+    Doctor.create!(
+      name: Faker::Name.name,
+      specialty: specialties.sample,
+      email: Faker::Internet.unique.email,
+      hospital: hospital
+    )
+  end
+
+  # Departments
+  3.times do
+    Department.create!(
+      name: Faker::Company.unique.name,
+      hospital: hospital
+    )
+  end
+
+  # Reviews
+  6.times do
+    Review.create!(
+      rating: rand(1..5),
+      comment: Faker::Lorem.sentence(word_count: 12),
+      hospital: hospital
+    )
+  end
+end
