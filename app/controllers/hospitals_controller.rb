5f0338545dd7ef334d5eb3da97957732fd5cdac0
Ssimran-5 <simranbhumbak22@gmail.com>
2026-03-22 22:55:46 -0500

    modified index.html.erb and hospital_controller.rb

--- app/controllers/hospitals_controller.rb
+++ app/controllers/hospitals_controller.rb
@@ -1,14 +1,19 @@
 class HospitalsController < ApplicationController
   def index
     hospitals = if params[:query].present?
-                  Hospital.where(
-                    "name LIKE ? OR city LIKE ?",
-                    "%#{params[:query]}%",
-                    "%#{params[:query]}%"
+                Hospital
+                  .left_joins(:departments)
+                  .where(
+                    "hospitals.name LIKE :q
+                     OR hospitals.city LIKE :q
+                     OR departments.name LIKE :q",
+                    q: "%#{params[:query]}%"
                   )
-                else
-                  Hospital.all
-                end
+                  .distinct
+              else
+                Hospital.all
+              end
+
 
     @hospitals = hospitals.page(params[:page]).per(9)
   end
